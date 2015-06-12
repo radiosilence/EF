238f4a7caf6aefb2fdb7aad1ac587d008841fc4b
James Cleveland <jc@blit.cc>
2015-06-12 11:50:56 +0100

    fix bug

--- spider-img.js
+++ spider-img.js
@@ -11,38 +11,34 @@ const data = JSON.parse(fs.readFileSync('./ef.json'));
 
 const download = (uri, filename) => {
   return new Promise((resolve, reject) => {
-    request.head(uri, function(err, res, body) {
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-      if (fs.existsSync(filename)) {
-        resolve();
-      } else {
+    if (fs.existsSync(filename)) {
+      console.log("Skipping", uri);
+      resolve();
+    } else {
+      request.head(uri, function(err, res, body) {
+      
+        console.log("Downloading", uri);
         request(uri).pipe(fs.createWriteStream(filename)).on('close', resolve);  
-      }
-    });
+      });
+    }
   });
 };
 
 const spiderImg = (i, links) => {
   let url = URL.parse(links[i], true);
+  console.log(links[i]);
   request.get(`${API}${url.query.fbid}?access_token=${AT}`, (err, response, body) => {
-    console.log("GOT", JSON.parse(body));
     let data = JSON.parse(body);
-    if (data && data.image) {
+    console.log(data.images ? data.images.length : 'None');
+    if (data && data.images) {
       Promise.all(data.images.map(image => download(image.source, `img/${sha1(image.source)}.jpg`)))
         .then(() => {
-          console.log("Spidering post", i + 1);
           spiderImg(++i, links);
         });
     } else {
       spiderImg(++i, links);
     }
   });
-  console.log(url);
-  // if (!url) return;
-  // download(url, `img/${sha1(url)}.jpg`, () => {
-  //   spiderImg(++i, links);
-  // });
 }
 
 let urlId = {};
