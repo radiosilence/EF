e4d8ca2d970b6ff6a610bbe452d515ae10f68c7e
James Cleveland <jc@blit.cc>
2015-06-12 14:36:11 +0100

    resume support

--- spider-img.js
+++ spider-img.js
@@ -28,27 +28,37 @@ const spiderImg = (i, links) => {
   let url = URL.parse(links[i], true);
   if (i === links.length) {
     console.log("DONE!", Object.keys(posts).length, 'posts');
-    fs.writeFileSync('./posts.json', JSON.stringify(posts));
+    fs.writeFileSync('./posts.json', JSON.stringify(posts, null, 4));
     return;
   }
   console.log(links[i]);
-  request.get(`${API}${url.query.fbid}?access_token=${AT}`, (err, response, body) => {
-    let data = JSON.parse(body);
-    console.log(data.images ? data.images.length : 'None');
-    if (data && data.images) {
-      Promise.all(data.images.map(image => download(image.source, `img/${sha1(image.source)}.jpg`)))
-        .then(() => {
-          posts[links[i]] = data;
-          fs.writeFileSync('./posts.json', JSON.stringify(posts));
-          spiderImg(++i, links);
-        });
-    } else {
-      spiderImg(++i, links);
-    }
-  });
+  if (posts[links[i]] === undefined) {
+    request.get(`${API}${url.query.fbid}?access_token=${AT}`, (err, response, body) => {
+      let data = JSON.parse(body);
+      console.log(data.images ? data.images.length : 'None');
+      if (data && data.images) {
+        Promise.all(data.images.map(image => download(image.source, `img/${sha1(image.source)}.jpg`)))
+          .then(() => {
+            posts[links[i]] = data;
+            fs.writeFileSync('./posts.json', JSON.stringify(posts, null, 4));
+            spiderImg(++i, links);
+          });
+      } else {
+        console.log("duff data");
+        spiderImg(++i, links);
+      }
+    });    
+  } else {
+    console.log("Post", links[i], "already downloaded");
+    spiderImg(++i, links);
+  }
+
 }
 
 let posts = {};
+try {
+  posts = JSON.parse(fs.readFileSync('./posts.json'));
+} catch {}
 
 let urlId = {};
 let links = data
